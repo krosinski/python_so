63ca270c329f7f30b67f327022bc4b9971e37eef
krosinski <krz.rosinski@gmail.com>
2015-10-16 10:59:52 +0200

    cleanup build script

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-#gcc -o test_app.out -I/usr/include/python2.7 -lpython2.7 main.c
-rm test_app.bin test_app.pyc test_app.log AwesomeModule.so* 2>/dev/null
+rm sample_app.bin app.log AwesomeModule.so* *pyc 2>/dev/null
 clang++ -Ipy27_bin/include/python2.7/ -Lpy27_bin/lib/ py27_bin/lib/libpython2.7.a AwesomeModule.cpp -shared -o AwesomeModule.so.1.0
 clang++ -Ipy27_bin/include/python2.7/  sample_app.cpp -o sample_app.bin AwesomeModule.so.1.0
